15be426f985f2cd44ed5b7851076fa8b4247fb7e
DuHaiQing2021 <792482948@qq.com>
2021-10-09 16:09:02 +0800

    2021.10.9

---
+++ test21.10.10/src/testDemo.java
@@ -0,0 +1,9 @@
+public class testDemo {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int ret=a+b;
+        System.out.println("这个数是："+ret);
+
+    }
+}
