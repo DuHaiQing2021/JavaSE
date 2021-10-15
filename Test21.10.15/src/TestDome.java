33c134d13a0d56404b256308cf1ffff8d44d465a
DuHaiQing2021 <792482948@qq.com>
2021-10-15 22:18:17 +0800

    2021.10.13

---
+++ Test21.10.15/src/TestDome.java
@@ -0,0 +1,12 @@
+
+public class TestDome {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world！");
+        System.out.print("hello world!");
+        System.out.printf("%5d",8);
+
+    }
+}
