2b991149fdda092aab5b9aab4dabc2d52aa16353
DuHaiQing2021 <792482948@qq.com>
2021-12-09 23:36:56 +0800

    2021_12_9

---
+++ Test21_12_9/src/com/company/IOUtils.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.io.FileOutputStream;
+
+public class IOUtils {
+
+    public static void serOblject(Object obj,String filePath)throws Exception{
+        try(
+                FileOutputStream fos=new FileOutputStream(filePath);
+                Object
+                ){}
+    }
+}
---
+++ Test21_12_9/src/com/company/IOUtilsTest.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class IOUtilsTest {
+    public static void main(String[] args) {
+        //测试对象序列化
+        Student student=new Student();
+    }
+}
