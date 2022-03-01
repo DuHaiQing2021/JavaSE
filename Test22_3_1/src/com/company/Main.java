e611067776228db666e86295effd0b7935eb66b7
DuHaiQing2021 <792482948@qq.com>
2022-03-01 22:44:40 +0800

    2022_3_1

---
+++ Test22_3_1/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Date date=new Date();
+        System.out.println();
+
+
+    }
+
+    public static void main1(String[] args) {
+	// write your code here
+        int[] array={1,2,3,4,5};
+        System.out.println(Arrays.toString(array));
+
+
+    }
+}
